e4a836d1dbeff408c98996e80c83c30b5451a744
Trevor Mackin <tismetrm@gmail.com>
2023-04-03 06:39:29 -0700

    Sec 9. Styling components

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -19,6 +19,16 @@ class ActivityStore {
       .sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
   }
 
+  get groupedActivities() {
+    return Object.entries(
+        this.activitiesByDate.reduce((activities, activity) => {
+            const date = activity.date;
+            activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+            return activities;
+        }, {} as { [key: string]: IActivity[] })
+    )
+}
+
   loadActivities = async () => {
     this.setLoadingInitial(true);
     try {
---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,46 @@
+
+import { Link } from "react-router-dom";
+import { Item, Button, Segment, Icon } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+
+interface IProps {
+  activity: IActivity
+}
+
+const ActivityListItem = ({ activity }: IProps) => {
+  return (
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Image size='tiny' circular src='/assets/user.png' />
+            <Item.Content>
+              <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                {activity.title}
+              </Item.Header>
+              <Item.Description>Hosted by Bob</Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <Segment>
+        <span>
+          <Icon name='clock' /> {activity.date}
+          <Icon name='marker' /> {activity.venue}
+        </span>
+      </Segment>
+      <Segment clearing>
+        <span>{activity.description}</span>
+        <Button
+          as={Link}
+          to={`/activities/${activity.id}`}
+          content='View'
+          color='teal'
+          floated='right'
+        />
+      </Segment>
+    </Segment.Group>
+  )
+}
+
+export default ActivityListItem;
